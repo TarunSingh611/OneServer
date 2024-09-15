d1edf93326a09c9b1be79fc2ef05ba728a72ee00
Tarun Singh <TarunSingh611@protonmai.com>
2024-09-16 00:49:28 +0530

    err solved

--- routes/postRoutes.mjs
+++ routes/postRoutes.mjs
@@ -1,23 +1,22 @@
 import express from "express";
 import multer from "multer";
-export {postImage} from "../controllers/postController/postImage.mjs";
-export {userPost} from "../controllers/postController/postUserPost.mjs";
-// export {getLikes, deleteLikes, modifyLikes} from "../controllers/postController/postLikes.mjs";
-// export {getComments, postComment, changeComment, deleteComment} from "../controllers/postController/postComments.mjs";
-
+import { postImage } from "../controllers/postController/postImage.mjs";
+import { userPost } from "../controllers/postController/postUserPost.mjs";
+// Import other functions as needed
+// import { getLikes, deleteLikes, modifyLikes } from "../controllers/postController/postLikes.mjs";
+// import { getComments, postComment, changeComment, deleteComment } from "../controllers/postController/postComments.mjs";
 
 const router = express.Router();
-const upload = multer({dest:'public/'})
+const upload = multer({ dest: 'public/' });
 
-
+// Define routes
 router.post("/imagePost", upload.single("file"), postImage);
-
 router.get("/userPost", userPost);
 
+// Define other routes as needed
 // router.get("/likes", getLikes);
-// router.delete("/likes",deleteLikes);
+// router.delete("/likes", deleteLikes);
 // router.put("/likes", modifyLikes);
-
 // router.get("/comments", getComments);
 // router.post("/comments", postComment);
 // router.put("/comments", changeComment);
